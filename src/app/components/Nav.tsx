b3ee0855679c030b271584d13fd9f2ac60e33e07
minki3 <86992006+minki3@users.noreply.github.com>
2023-05-10 19:40:48 +0900

    ADD:네브바 구현

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiFillHome } from "react-icons/ai";
+import { BsPlusSquare } from "react-icons/bs";
+import { BsPlusSquareFill } from "react-icons/bs";
+import { RiSearchLine } from "react-icons/ri";
+import { RiSearchFill } from "react-icons/ri";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import ColorButton from "./ui/ColorButton";
+
+const CATEGORYS = [
+  { icon: <AiOutlineHome />, link: "/", fillIcon: <AiFillHome /> },
+  { icon: <RiSearchLine />, link: "/search", fillIcon: <RiSearchFill /> },
+  { icon: <BsPlusSquare />, link: "/new", fillIcon: <BsPlusSquareFill /> },
+];
+
+export default function Nav() {
+  const pathname = usePathname();
+  return (
+    <section className=" flex items-center p-5 border justify-between">
+      <Link href="/">
+        <h1 className=" text-2xl font-bold cursor-pointer">instangram</h1>
+      </Link>
+      <section className="flex items-center">
+        {CATEGORYS.map((item) => {
+          const { icon, link, fillIcon } = item;
+          return link === pathname ? (
+            <Link key={link} href={link} className="m-2 text-2xl">
+              {fillIcon}
+            </Link>
+          ) : (
+            <Link key={link} href={link} className="m-2 text-2xl">
+              {icon}
+            </Link>
+          );
+        })}
+        <ColorButton text="Sign in" onClick={() => {}} />
+      </section>
+    </section>
+  );
+}
